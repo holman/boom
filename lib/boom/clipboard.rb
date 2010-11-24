9edb208b1cb31aaf5b5df79bfc13452d22213bb6
Zach Holman <github.com@zachholman.com>
2010-11-24 00:35:09 -0800

    vim gets too aggressive with wrapping sometime. like kanye west.

--- lib/boom/clipboard.rb
+++ lib/boom/clipboard.rb
@@ -1,7 +1,9 @@
 # Clipboard is a centralized point to shell out to each individual platform's
 # clipboard, pasteboard, or whatever they decide to call it.
 #
-module Boom class Clipboard class << self
+module Boom
+  class Clipboard
+    class << self
       
       # Public: copies a given Item's value to the clipboard. This method is
       # designed to handle multiple platforms.
