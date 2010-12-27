c80b819b8724bfebae17df801a4546597f2b5bd4
Austin Bales <arbales@gmail.com>
2010-12-27 02:42:28 -0500

    Documentation for boom/capistrano

--- lib/boom/capistrano.rb
+++ lib/boom/capistrano.rb
@@ -6,7 +6,7 @@ end
 
 Capistrano::Configuration.instance(true).load do
 
-  def include_boom(list_name)
+  def boom(list_name)
     list = Boom::List.find(list_name)
     if list
       list.to_hash.flatten[1].each do |item|
@@ -19,8 +19,22 @@ Capistrano::Configuration.instance(true).load do
 
 end
 
-# E.g, to set environment variables from a boom list.
+# E.g, to set environment variables from a boom list.    
+#      
+#   ## from your local shell
 #
+#   boom mailapp
+#   boom mailapp postmark_api_key ca8b84aab0c88ce7a459650f6a17828199ab
+#   boom mailapp postmark_from_address no-replies@app_name.co
+#                        
+#   ## in config/deploy.rb
+# 
 #   require 'boom/capistrano'
-#   include_boom app_name
-#
+#   boom mailapp
+#                    
+#   ## in app/mailer.rb
+#                
+#   Postmark.api_key = ENV['MAILAPP_POSTMARK_API_KEY']
+#   ...
+#   message.from = ENV['MAILAPP_POSTMARK_FROM_ADDRESS']
+#    
