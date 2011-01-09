94600bd86c1361053675f97a272cd67af94aabd0
Zach Holman <github.com@zachholman.com>
2011-01-09 14:52:17 -0800

    namepsace backends

--- lib/boom.rb
+++ lib/boom.rb
@@ -14,20 +14,17 @@ require 'boom/clipboard'
 require 'boom/command'
 require 'boom/item'
 require 'boom/list'
-require 'boom/base'
-require 'boom/json'
-require 'boom/mongodb'
+
+require 'boom/storage'
+require 'boom/storage/base'
+require 'boom/storage/json'
+require 'boom/storage/mongodb'
 
 module Boom
   VERSION = '0.0.8'
 
-  def self.adapter
-    #@adapter ||= Boom::Storage::MongoDB.new
-    @adapter ||= Boom::Storage::JSON.new
-  end
-
   def self.storage
-    @storage ||= Boom::Storage::Base.new
+    @storage ||= Boom::Storage.backend
   end
   
 end
---
+++ lib/boom/storage.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+#
+# Storage is the interface between multiple Backends. You can use Storage
+# directly without having to worry about which Backend is in use.
+#
+module Boom
+  module Storage
+    
+    def self.backend=(backend)
+      # stub
+    end
+
+    def self.backend
+      Boom::Storage::JSON.new
+    end
+
+  end
+end
