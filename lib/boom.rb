d9ec6271080c96e5805d250c50e981f65327e30b
Erik Kastner <kastner@gmail.com>
2011-01-10 12:56:36 -0500

    1.8.6 support

--- lib/boom.rb
+++ lib/boom.rb
@@ -23,3 +23,11 @@ module Boom
     @storage ||= Boom::Storage.new
   end
 end
+
+unless Symbol.method_defined?(:to_proc)
+  class Symbol
+    def to_proc
+      Proc.new { |obj, *args| obj.send(self, *args) }
+    end
+  end
+end
