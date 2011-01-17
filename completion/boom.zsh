fc586ec4d89f414e83c1f1dcd332d548954f8206
Zach Holman <github.com@zachholman.com>
2011-01-17 00:04:50 -0800

    jimmy wales everybody

---
+++ completion/boom.zsh
@@ -0,0 +1,18 @@
+# oh hai!
+#
+# someone should fork this and add autocompletion for zsh
+#
+# you'd be pretty cool
+#
+# and I'd give you a high-five
+#
+#
+#
+# and maybe a whiskey
+#
+#
+#
+#
+#
+#
+# a couple of whiskeys
