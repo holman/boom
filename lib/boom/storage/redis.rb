9540f94189657d15dc1aafa0b205884d171fe800
Zach Holman <github.com@zachholman.com>
2011-01-28 12:36:06 -0800

    add redis stubs with new interface

---
+++ lib/boom/storage/redis.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+#
+# Sup, Redis.
+#
+module Boom
+  module Storage
+    class Redis < Base
+
+      def bootstrap
+      end
+
+      def setup
+      end
+
+      def save
+      end
+
+    end
+  end
+end
