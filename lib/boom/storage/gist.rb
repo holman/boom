b663e2ec19894676ecee87308ae040958d177a73
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-07-17 05:49:11 -0700

    implement save method for Gist backend

--- lib/boom/storage/gist.rb
+++ lib/boom/storage/gist.rb
@@ -63,7 +63,7 @@ module Boom
         end
 
         @storage = JSON.parse(response["files"]["boom.json"]["content"]) if response["files"] and response["files"]["boom.json"]
-        Boom.config.attributes["gist"]["gist_id"] = response["id"]
+        Boom.config.attributes["gist"]["gist_id"] = @gist_id = response["id"]
         Boom.config.save
 
         unless @storage
@@ -87,7 +87,23 @@ module Boom
       end
 
       def save
+        params = {
+          :basic_auth => {
+            :username => @username,
+            :password => @password
+          },
+          :body => JSON.generate({
+            :description => "Data for Boom",
+            :public => false,
+            :files => {
+              "boom.json" => {
+                "content" => JSON.generate(to_hash)
+              }
+            }
+          })
+        }
 
+        response = self.class.post("https://api.github.com/gists/#{@gist_id}", params)
       end
     end
   end
