293244665fcf2f81952112b6c0b8d885ba2dafd4
Antonio Lobato <alobato@jinked.com>
2011-10-28 10:29:41 -0400

    Modified MongoDB storage driver so each list is stored in an individual document for easier web interface access, etc.

--- lib/boom/storage/mongodb.rb
+++ lib/boom/storage/mongodb.rb
@@ -8,77 +8,76 @@ end
 
 module Boom
   module Storage
-    class Mongodb < Base
-      
-      # Public: Initialize MongoDB connection and check dep.
-      #
-      # Returns Mongo connection
-      def mongo
-        @mongo ||= ::Mongo::Connection.new(
-          Boom.config.attributes["mongodb"]["host"],
-          Boom.config.attributes["mongodb"]["port"]
-        ).db(Boom.config.attributes["mongodb"]["database"])
-        
-        @mongo.authenticate(
-          Boom.config.attributes['mongodb']['username'], 
-          Boom.config.attributes['mongodb']['password']
-        )
-      
-        # Return connection
-        @mongo
-      rescue NameError => e
-        puts "You don't have the Mongo gem installed yet:\n  gem install mongo"
-       exit
-      end
-      
-      # Public: The MongoDB collection
-      #
-      # Returns the MongoDB collection
-      def collection
-        @collection ||= mongo.collection(Boom.config.attributes["mongodb"]["collection"])
-      end
-      
-      # Public: Bootstrap
-      #
-      # Returns
-      def bootstrap
-        collection.insert("boom" => '{"lists": [{}]}') if collection.find_one.nil?
-      end   
+	class Mongodb < Base
+	  
+	  # Public: Initialize MongoDB connection and check dep.
+	  #
+	  # Returns Mongo connection
+	  def mongo
+		@mongo ||= ::Mongo::Connection.new(
+		  Boom.config.attributes["mongodb"]["host"],
+		  Boom.config.attributes["mongodb"]["port"]
+		).db(Boom.config.attributes["mongodb"]["database"])
+		
+		@mongo.authenticate(
+		  Boom.config.attributes['mongodb']['username'], 
+		  Boom.config.attributes['mongodb']['password']
+		)
+	  
+		# Return connection
+		@mongo
+	  rescue NameError => e
+		puts "You don't have the Mongo gem installed yet:\n  gem install mongo"
+	   exit
+	  end
+	  
+	  # Public: The MongoDB collection
+	  #
+	  # Returns the MongoDB collection
+	  def collection
+		@collection ||= mongo.collection(Boom.config.attributes["mongodb"]["collection"])
+	  end
+	  
+	  # Public: Bootstrap
+	  #
+	  # Returns
+	  def bootstrap
+	  end	
 
-      # Public: Populates the memory list from MongoDB
-      #
-      # Returns nothing
-      def populate
-        storage = MultiJson.decode(collection.find_one['boom']) || []
-              
-        storage['lists'].each do |lists|
-          lists.each do |list_name, items|
-            @lists << list = List.new(list_name)
-
-            items.each do |item|
-              item.each do |name,value|
-                list.add_item(Item.new(name,value))
-              end
-            end
-          end
-        end   
-      end
-      
-      # Public: Save to MongoDB
-      #
-      # Returns Mongo ID
-      def save
-        doc = collection.find_one()
-        collection.update({"_id" => doc["_id"]}, {'boom' => to_json})
-      end
-      
-      # Public: Convert to JSON
-      #
-      # Returns
-      def to_json
-       MultiJson.encode(to_hash)
-      end
-      
-    end
+	  # Public: Populates the memory list from MongoDB
+	  #
+	  # Returns nothing
+	  def populate
+		storage = collection.find() || Hash.new
+		storage.each do |doc|
+			list = MultiJson.decode(doc['boom'])
+			list.each do |list_name, items|
+				@lists << list = List.new(list_name)
+				items.each do |item|
+					item.each do |name,value|
+						list.add_item(Item.new(name,value))
+					end
+				end
+			end
+		end
+	  end
+	  
+	  # Public: Save to MongoDB
+	  #
+	  # Returns Mongo ID
+	  def save
+		lists.each do |list|
+			collection.update({"_id" => list.name}, {"boom" => MultiJson.encode(list.to_hash), "_id" => list.name}, {:upsert => true})
+		end
+	  end
+	  
+	  # Public: Convert to JSON
+	  #
+	  # Returns
+	  def to_json
+	   MultiJson.encode(to_hash)
+	  end
+	  
+	end
   end
 end
