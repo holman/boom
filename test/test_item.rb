e85363499008d412255eb33f46bc785f2306f305
Adam Moore <nerdfunk@gmail.com>
2012-01-25 22:16:49 +0900

    Add item tests for move

--- test/test_item.rb
+++ test/test_item.rb
@@ -41,14 +41,33 @@ class TestItem < Test::Unit::TestCase
   def test_url
     assert_equal 'https://github.com', @item.url
   end
-  
+
   def test_url_with_additional_description
     @item = Boom::Item.new('github', 'social coding https://github.com')
     assert_equal 'https://github.com', @item.url
   end
-  
+
   def test_url_without_url
     @item = Boom::Item.new('didum', 'dadam lol omg')
     assert_equal 'dadam lol omg', @item.url
   end
+
+  def test_move_item_to_new_list
+    new_list_name = "urls"
+
+    @item.move(new_list_name)
+    @list = Boom::List.find(new_list_name)
+
+    assert_equal 1, @list.items.size
+  end
+
+  def test_move_item_to_existing_list
+    @list_2 = Boom::List.new('sexy-companies')
+
+    @item.move(@list_2.name)
+
+    assert_equal 1, @list_2.items.size
+    assert_equal 0, @list.items.size
+  end
+
 end
