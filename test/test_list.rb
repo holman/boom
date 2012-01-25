f32c99881b1ba8d391bd3de48073777c36c422fa
Adam Moore <nerdfunk@gmail.com>
2012-01-25 22:36:11 +0900

    Change to list method, add tests

--- test/test_list.rb
+++ test/test_list.rb
@@ -76,4 +76,21 @@ class TestList < Test::Unit::TestCase
     assert_equal 1, @list_2.items.size
   end
 
+  def test_move_item_to_new_list
+    new_list_name = "urls"
+
+    @list_2.move(@item.name, new_list_name)
+
+    @list_3 = Boom::List.find(new_list_name)
+    assert_equal 1, @list_3.items.size
+    assert_equal 0, @list_2.items.size
+  end
+
+  def test_move_item_to_existing_list
+    @list_3.move(@item.name, @list_2.name)
+
+    assert_equal 1, @list_2.items.size
+    assert_equal 0, @list_3.items.size
+  end
+
 end
