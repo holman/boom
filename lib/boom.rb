8c93262097a7b3b828fca71141afd8b91dca739f
Zach Holman <zach@zachholman.com>
2013-03-15 15:50:59 -0700

    Get rid of multiple backends

--- lib/boom.rb
+++ lib/boom.rb
@@ -17,27 +17,11 @@ require 'boom/config'
 require 'boom/item'
 require 'boom/list'
 
-require 'boom/storage'
-require 'boom/storage/base'
-require 'boom/storage/json'
-require 'boom/storage/redis'
-require 'boom/storage/mongodb'
-require 'boom/storage/keychain'
-require 'boom/storage/gist'
-
 require 'boom/core_ext/symbol'
 
 module Boom
   VERSION = '0.2.4'
 
-  extend self
-
-  def storage
-    @storage ||= Boom::Storage.backend
+  def self.storage
   end
-
-  def config
-    @config ||= Boom::Config.new
-  end
-
 end
