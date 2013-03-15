28a3a2f1143af3a3ab63e032df9aa53f9d64b6f1
Zach Holman <zach@zachholman.com>
2013-03-15 15:39:35 -0700

    Ruby tests are stupid. Use shell.

---
+++ test/cli.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env roundup
+
+describe "cli: tests around boom the binary — help, version, and so on"
+
+boom="./bin/boom"
+
+it_shows_help() {
+  $boom help | grep "boom: help"
+}
+
+it_shows_a_version() {
+  $boom --version | grep "running boom"
+}
