fef53c5c5fef9b2d4e07c8494ca153961dc46d06
Eugene Kenny <elkenny@gmail.com>
2013-04-03 22:57:14 +0100

    Make delete a top level command

--- lib/boom/command.rb
+++ lib/boom/command.rb
@@ -96,19 +96,20 @@ module Boom
         return open(major,minor) if command == 'open' || command == 'o'
         return random(major)     if command == 'random' || command == 'rand' || command == 'r'
 
-        # if we're operating on a List
-        if storage.list_exists?(command)
-          return delete_list(command) if major == '--delete'
-          return detail_list(command) unless major
-          unless minor == '--delete'
-            return add_item(command,major,minor) if minor
-            return add_item(command,major,stdin.read) if stdin.stat.size > 0
-            return search_list_for_item(command, major)
+        if command == 'delete' || command == 'd'
+          if minor
+            return delete_item(major, minor)
+          else
+            return delete_list(major)
           end
         end
 
-        if minor == '--delete' and storage.item_exists?(major)
-          return delete_item(command, major)
+        # if we're operating on a List
+        if storage.list_exists?(command)
+          return detail_list(command) unless major
+          return add_item(command,major,minor) if minor
+          return add_item(command,major,stdin.read) if stdin.stat.size > 0
+          return search_list_for_item(command, major)
         end
 
         return search_items(command) if storage.item_exists?(command) and !major
